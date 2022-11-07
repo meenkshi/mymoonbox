94d2902fcab71d7946613b62d353ebe53b1bd70e
kamalkj9460 <79216365+kamalkj9460@users.noreply.github.com>
2022-11-07 19:10:01 +0530

    optimized theme
    optimized theme

--- assets/ticker.js
+++ assets/ticker.js
@@ -1,7 +1,10 @@
-$('.marquee').marquee({
+setTimeout(function() {
+    var $ = jQuery;
+	$('.marquee').marquee({
     pauseOnHover: true,
     allowCss3Support: false,
     duration: 30000,
     css3easing: false,
     startVisible: true
-  });
+      });
+},2000);
